ff6d60b55996f9a52ac635ec89eb85ed66d54925
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2023-08-07 07:47:10 -0400

    wip: fetch

--- src/data/artistsAPI.js
+++ src/data/artistsAPI.js
@@ -4,33 +4,37 @@ const authURL = process.env.AUTH_URL;
 const baseURL = process.env.BASE_URL;
 const clientID = process.env.CLIENT_ID;
 const client_secret = process.env.SECRET_ID;
-const cors = "https://cors-anywhere.herokuapp.com/";
+const ngrok = " https://97fc-190-6-42-120.ngrok-free.app";
+// const cors = "https://cors-anywhere.herokuapp.com/";
 
 // Para solucionar el problema de CORS:
 // Coloca "https://cors-anywhere.herokuapp.com/" + URL
 // PD: SI te sigue saliendo el error, debes entrar a https://cors-anywhere.herokuapp.com/ y permitir acceso
 
 const clientApi = axios.create({
-  baseURL: cors + baseURL,
+  baseURL,
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
-  body: `grant_type=credentials&client=${clientID}&client_secret${client_secret}`,
+  body: `grant_type=credentials&client=${clientID}&client_secret=${client_secret}`,
 });
 
 const authOptions = axios.create({
-  baseURL: authURL,
-  mode: "same-origin",
+  baseURL: `${ngrok}/${authURL}`,
+  // mode: "same-origin",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
-  body: `grant_type=credentials&client=${clientID}&client_secret${client_secret}`,
+  body: `grant_type=credentials&client=${clientID}&client_secret=${client_secret}`,
 });
 
-export const getAuthentication = () => {
-  authOptions.post("/api/token").then((data) => {
-    console.log(data);
-  });
+export const getAuthentication = async () => {
+  try {
+    const data = await authOptions.post("");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getArtist = async () => {
